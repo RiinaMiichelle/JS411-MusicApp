e4e617296116b49d0d7f3920155616649b6a8b7c
RiinaMiichelle <sarina.m.colosimo@gmail.com>
2020-03-31 20:05:19 -0500

    starting musicApp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './musicApp';
+import App from './musicApp';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
